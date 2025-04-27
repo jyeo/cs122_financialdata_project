db4100e4cbe8fc5babc4cfa8f548df677d12fa62
“ambiguus <y03.jasp3r@gmail.com>
2025-04-27 00:06:05 -0700

    Quaesitatum. Sublatum. Renovatum.
    安装简体汉字
    
    别让工具生锈

--- code/utils/visualize.py
+++ code/utils/visualize.py
@@ -3,13 +3,36 @@ matplotlib.use("Agg")  # Use the Agg backend for non-GUI rendering
 import matplotlib.pyplot as plt
 import os
 
+font_path = "NotoSansSC-Regular.otf"
+
+current_dir = os.path.dirname(__file__)
+font_path = os.path.join(current_dir, "../static/fonts/NotoSansSC-Regular.ttf")
+
+from matplotlib import font_manager
+
+font_prop = font_manager.FontProperties(fname=font_path)
+plt.rcParams["font.family"] = font_prop.get_name()
+
+matplotlib.rcParams['axes.unicode_minus'] = False
+
+
 def plot_popularity_bar(df, artist_name):
     sorted_df = df.sort_values("popularity", ascending=True)
     plt.figure(figsize=(10, 6))
-    plt.barh(sorted_df["track_name"], sorted_df["popularity"], color="skyblue")
-    plt.title(f"Track Popularity – {artist_name}")
-    plt.xlabel("Popularity")
+
+    plt.barh(
+        sorted_df["track_name"],
+        sorted_df["popularity"],
+        color="skyblue"
+    )
+
+    plt.title(f"Track Popularity – {artist_name}", fontproperties=font_prop, fontsize=16)
+    plt.xlabel("Popularity", fontproperties=font_prop, fontsize=14)
     plt.xlim(0, 100)
+
+    ax = plt.gca()
+    ax.set_yticklabels(sorted_df["track_name"], fontproperties=font_prop, fontsize=12)
+
     plt.tight_layout()
 
     # Create the static folder inside the code directory if it doesn't exist
